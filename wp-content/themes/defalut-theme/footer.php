075fd664e9e64f2a669305c23076d84d433601a9
edertonlima <M0needer!@>
2020-06-22 14:52:54 -0300

    Instalação

---
+++ wp-content/themes/defalut-theme/footer.php
@@ -0,0 +1,31 @@
+		
+		<footer class="footer">
+			<div class="container">
+				
+				<div class="row">
+					<div class="col-6">
+						<h3>Esta página foi útil?</h3>
+					</div>
+					<div class="col-6">
+						<div class="ico-satisfacao">
+							<a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-satisfacao-1.png" alt=""></a>
+							<a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-satisfacao-2.png" alt=""></a>
+							<a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-satisfacao-3.png" alt=""></a>
+							<a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-satisfacao-4.png" alt=""></a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</footer>
+
+		<?php wp_footer(); ?>
+
+		<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+
+		<script type="text/javascript">
+		</script>
+
+	</body>
+</html>
---
+++ wp-content/themes/defalut-theme/front-page.php
@@ -0,0 +1,300 @@
+<?php get_header(); ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<section class="box-section">
+			<div class="container">
+				<div class="row flex align-center mobile-reverse">
+					
+					<div class="col-5">
+						<h2>
+							Reembolsos corporativos de forma simples e rápida.
+							<br>
+							<strong>Como deve ser.</strong>
+						</h2>
+
+						<p class="cont-recuo">Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando aprovações, visualizando relatórios e muito mais!</p>
+
+						<a href="" class="btn cor2" title="Peça já sua proposta">Peça já sua proposta</a>
+
+						<p class="cor2 center-mobile margin-bottom-30px">Responderemos em até 24h.</p>
+					</div>
+					<div class="col-7">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/large-Header_3d_3_1.png" alt="Reembolsos corporativos de forma simples e rápida. Como deve ser">
+					</div>
+
+				</div>
+
+				<div class="row flex align-center">
+
+					<div class="col-7">
+						<a data-fancybox href="https://www.youtube.com/watch?v=OvtUAuyL6b4" class="video-home" title="Assistir ao vídeo">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/play-solid.png" alt="Assistir ao vídeo">
+						</a>
+					</div>
+					<div class="col-5">
+						<h2>
+							Assuma agora o 
+							<br>
+							<strong>controle dos gostos</strong>
+							variáveis da sua empresa
+						</h2>
+
+						<p>O BPP Corp é uma solução única para a administração de gastos corporativos que conta com uma conta digital empresarial grátis, cartões VISA pré-pagos para controle total das despesas e um aplicativo sem custos adicionais para tornar seu controle financeiro mais simples e prático! </p>
+
+						<a href="" class="btn cor2 mobile-inline" title="Peça já sua proposta">Peça já sua proposta</a>
+					</div>
+
+				</div>
+			</div>
+		</section>
+
+		<section class="box-section">
+			<div class="container">
+
+				<div class="banner-destaque">
+					<div class="row flex align-bottom">
+
+						<div class="col-m-1 col-4">
+							<h3>
+								<strong>Organizar<br>reembolsos</strong> da sua empresa nunca foi tão rápido
+							</h3>
+
+							<p class="">Utilizando BPP Corp, você tem controle total sobre o budget de sua equipe podendo realizar transferências instantâneas para os cartões corporativos dos colaboradores, acompanhar gastos, receber comprovantes e visualizar relatórios que organizam seu reembolso.</p>
+
+							<a href="" class="btn cor1 mobile-inline-show" title="Peça já sua proposta">Peça já sua proposta</a>
+						</div>
+						<div class="col-m-1 col-6">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bpp_corp_reembolso.png" alt="Assistir ao vídeo">
+						</div>
+
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+		<section class="box-section">
+			<div class="container">
+
+				<div class="itens-ico">
+
+					<h3 class="center">
+						A plataforma que ajuda a <strong>reduzir os gastos</strong> do dia a dia
+					</h3>
+
+					<p class="subtitulo">Simplifique a gestão das despesas, evitando a perda de tempo com comprovantes e planilhas, reduzindo custos e aumentando os resultados da sua empresa.</p>
+
+						<div class="row owl-carousel owl-theme owl-loaded" id="itens-ico">
+							<div class="owl-stage-outer">
+								<div class="owl-stage"> 
+
+							<div class="col-4 item owl-item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico1.png" alt="Mais rapidez na gestão das suas despesas">
+								<h4>Mais rapidez na gestão das suas despesas</h4>
+								<p>Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados de reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo isso de forma rápida e digital. </p>
+							</div>
+
+							<div class="col-4 item owl-item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico2.png" alt="">
+								<h4>Os relatórios que você precisa em um único lugar</h4>
+								<p>Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em sua equipe. Confira o saldo de cada cartão, redistribua valores, faça transferências instantâneas e não perca tempo com formulários e planilhas. </p>
+							</div>
+
+							<div class="col-4 item owl-item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico3.png" alt="">
+								<h4>Organize todos os seus comprovantes</h4>
+								<p>Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não precisa mais guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser classificados por tipo ou por projeto, da forma que você precisar.</p>
+							</div>
+							<div class="col-4 item owl-item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico4.png" alt="">
+								<h4>Apresente suas despesas com um só clique</h4>
+								<p>Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma foto do cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das despesas pode aprovar ou reprovar aquele gasto através do App.</p>
+							</div>
+							<div class="col-4 item owl-item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico5.png" alt="">
+								<h4>Controle seus gastos com cartões pré-pagos VISA</h4>
+								<p>Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa disponibiliza para compras online ou em lojas físicas. Conte com cartões VISA aceitos em mais de 30 milhões de estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+							</div>
+							<div class="col-4 item owl-item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico6.png" alt="">
+								<h4>Conta digital gratuita e exclusiva para sua empresa</h4>
+								<p>Oferecemos uma conta digital gratuita especialmente para gerenciar despesas.  Transfira o saldo de sua conta para os cartões de maneira instantânea, sempre livre de taxas de manutenção ou de juros.</p>
+							</div>
+							
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+
+
+		<section class="box-section marcas">
+			<div class="container">
+				
+				<div class="row flex align-center mobile-reverse no-margin">
+					
+					<div class="col-m-1 col-5">
+						<div class="ico-marcas">
+							<span><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-marcas-1.png" alt=""></span>
+							<span><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-marcas-2.png" alt=""></span>
+							<span><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-marcas-3.png" alt=""></span>
+							<span><img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-marcas-4.png" alt=""></span>
+						</div>
+					</div>
+
+					<div class="col-m-1 col-4">
+						<h3>
+							Conheça quem já conta com as <strong>vantagens excluisivas</strong> do BPP Corp
+						</h3>
+
+						<p class="cont-recuo margin">A empresas mais bem-sucedidas do mundo, grandes e pequenas, confiam no BPP Corp para reduzir custos com organização dos gastos da suas empresas. </p>
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+
+
+		<section class="box-section">
+			<div class="container">
+
+				<h3 class="center">
+					Compare e <strong>decida agora</strong> mesmo
+				</h3>
+
+				<p class="subtitulo">Substitua o uso do dinheiro em espécie pelo BPP Corp e ganhe segurança e praticidade, evitando roubos e perdas, tendo uma plataforma única para gerir suas despesas. </p>
+
+				<div class="list-txt-home">
+					<div class="row">
+						<div class="col-m-1 col-5 col-left">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-list-txt-1.png" alt="">
+							<h3>Dinheiro em papel</h3>
+							<ul>
+								<li>Saque de dinheiro em papel para as despesas </li>
+								<li>Várias notas fiscais em papel</li>
+								<li>Diversas planilhas de gestão</li>
+								<li>Problemas de contabilização dos gastos</li>
+								<li>Não tem segurança</li>
+							</ul>
+						</div>
+
+						<div class="col-5 col-right">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico-list-txt-2.png" alt="">
+							<h3>BPP Corp</h3>
+							<ul>
+								<li>Faça pagamentos com o BPP Corp sem precisar tirar dinheiro</li>
+								<li>Prestação de contas sem papel, via app</li>
+								<li>Relatórios completos em um único lugar</li>
+								<li>Controle dos gastos com cartões pré-pago</li>
+								<li>Segurança total do seu dinheiro</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+
+		<section class="box-section">
+			<div class="container">
+
+				<div class="bg-imagem banner-destaque-full" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/bg-banner-full.jpg');">
+
+					<div class="row">
+						<div class="col-12 center">
+							<h3>A sua gestão financeira nunca foi tão fácil.</h3>
+
+							<p class="">Conte com as soluções do BPP Corp e facilite o controle dos gastos da sua empresa agora mesmo.</p>
+
+							<a href="" class="btn cor2" title="Peça já sua proposta">Peça já sua proposta</a>
+
+							<p class="txt-mini">Proposta <strong>grátis</strong> e sem compromisso.</p>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+
+
+		<?php
+		$query = array(
+				'post_type' => 'post',
+				'posts_per_page' => 3
+			);
+		query_posts( $query );
+
+		if( have_posts() ){ ?>
+			<section class="box-section post list-post">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-m-1 col-10">
+
+							<h2>Últimos posts</h2>
+
+							<div class="row row-mini flex">
+								
+								<?php 
+								while ( have_posts() ) : the_post();
+									get_template_part( 'content' );
+								endwhile; 
+								wp_reset_query();
+								?>
+
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+			</section>
+		<?php } ?>
+ 
+	<?php endwhile; ?>
+
+<?php get_footer(); ?>
+
+<!-- CAROUSEL -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/owl-carousel/owl.carousel.min.js"></script>
+
+<script type="text/javascript">
+
+	$('#itens-ico').owlCarousel({
+		loop:false,
+		margin:0,
+		responsiveClass:true,
+		dots:true,
+		nav:false,
+		//rtl:true,
+		responsive:{
+			0:{
+				nav:false,
+				items:1
+			},
+			680:{
+				nav:false,
+				items:2
+			},
+			1180:{
+				nav:false,
+				items:3
+			},
+			1181:{
+				nav:false,
+				items:6,
+				dots:false,
+			}
+		}
+	})
+	
+</script>
